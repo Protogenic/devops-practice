cac9c77a945a1fc68868f678c64bdcfb9a0ff103
Владимир Безруков <82569672+Protogenic@users.noreply.github.com>
2024-04-05 11:13:14 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,10 +3,20 @@ from main import add, subtract
 
 class TestMain(unittest.TestCase):
 
-    def test_add(self):
+    def test_add1(self):
         self.assertEqual(add(3, 5), 8)
+
+    def test_add2(self):
         self.assertEqual(add(-1, 1), 0)
+        
+    def test_add3(self):
         self.assertEqual(add(0, 0), 0)
+        
+    def test_add4(self):
+        self.assertEqual(add(10, -5), 5)
+
+    def test_add5(self):
+        self.assertEqual(add(2.5, 3.5), 6)
 
 if __name__ == '__main__':
     unittest.main()
